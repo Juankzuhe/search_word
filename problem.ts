bc5deb9d272be08158b4d8d82a5af87a2d73faba
juankzuluaga@gmail.com <juanplay14>
2020-11-29 19:24:30 -0500

    upload exercise

---
+++ problem.ts
@@ -0,0 +1,20 @@
+let text = "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas 'Letraset', las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum."
+
+function normalize(word: string) {
+    return word.toLowerCase().replace(/[.,!)(']/g, "")
+}
+
+function wordRepetitions(text: string) {
+    let dict: {[key: string]: number} = {};
+    let separatedWords = text.split(" ");
+    for (let word of separatedWords) {
+        if (normalize(word) in dict) {
+            ++dict[normalize(word)];
+        } else {
+            dict[normalize(word)] = 1;
+        }
+    }
+    console.log(dict);
+}
+
+wordRepetitions(text);
